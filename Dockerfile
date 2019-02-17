b859a7525386759579021cce3c66df8f760bb7d9
Chris Miller <chrisamiller@gmail.com>
2019-02-17 16:03:51 -0600

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM griffithlab/vatools
+
+LABEL \
+    description="tools needed for mapq0 filter"
+
+##########
+#GATK 3.6#
+##########
+RUN apt-get update -y && apt-get install -y \
+    apt-utils \
+    bzip2 \
+    default-jre \
+    wget \
+    zlib1g-dev #for pysam
+
+RUN cd /tmp/ \
+    && wget -O /tmp/gatk3.6.tar.bz2 'https://software.broadinstitute.org/gatk/download/auth?package=GATK-archive&version=3.6-0-g89b7209' \
+    && tar xf gatk3.6.tar.bz2 \
+    && cp GenomeAnalysisTK.jar /opt/GenomeAnalysisTK.jar \
+    && rm -rf /tmp/*
+
+##############
+#mapq0 filter
+##############
+COPY mapq0_vcf_filter.sh /usr/bin/mapq0_vcf_filter.sh
+RUN chmod +x /usr/bin/mapq0_vcf_filter.sh
+RUN pip3 install pysam
+RUN pip3 install pysamstats
---
+++ mapq0_vcf_filter.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+outvcf=$1
+vcf=$2
+bam=$3
+ref_fasta=$4
+mapq0perc=$5
+outdir=$(dirname "$outvcf")
+
+#grab sites that don't already have the MQ0 field
+count=0;
+zgrep -v "^#" "$vcf" | grep -v "MQ0" | cut -f 1,2 | while read chr pos;do
+    pysamstats --type mapq --chromosome $chr --start $pos --end $((pos+1)) "$bam"  | grep $pos | cut -f 1,2,5 >>$outdir/mapq0counts
+    count=$((count+1))
+done
+
+if [[ $count -eq 0 ]];then
+    #no sites to process, just copy the vcf
+    cp $vcf $outdir/mapq0.vcf
+else 
+    #need to add MQ0
+    
+    #does the file contain the MQ0 field already?
+    mqcount=0
+    case "$vcf" in
+        *.gz | *.tgz )
+            #gzipped vcf
+            mqcount=$(gunzip -c "$vcf" | grep "^#" | grep -w MQ0 | wc -l)
+            ;;
+        *)
+            #non-gzipped vcf
+            mqcount=$(grep "^#" "$vcf" | grep -w MQ0 | wc -l)
+            ;;
+    esac
+    
+    if [[ $mqcount -gt 0 ]];then
+        #already has mq0 set, we're all good
+        vcf-info-annotator --overwrite -o $outdir/mapq0.vcf "$vcf" $outdir/mapq0counts MQ0 
+    else
+        #no mq0, need to set the header line as well
+        vcf-info-annotator -o $outdir/mapq0.vcf -f Integer -d "Number of MAPQ == 0 reads covering this record" "$vcf" $outdir/mapq0counts MQ0
+    fi
+fi
+#finally, set the filter tags on the vcf
+#the multiplication by 1.0 is necessary to convert integers to floats before dividing in the JEXL expression 
+#(which is dumb, and I want an hour of my life back)
+java -Xmx6g -jar /opt/GenomeAnalysisTK.jar -T VariantFiltration -R $ref_fasta -o $outdir/mapq_filtered.vcf.gz --variant $outdir/mapq0.vcf --filterExpression "((MQ0*1.0) / (DP*1.0)) > $mapq0perc" --filterName "MAPQ0"
